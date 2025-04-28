48ed5b2c02be6327144cbfae65f6c454b441ce01
João Vitor Alves Barroso <jvabarroso@gmail.com>
2025-04-28 13:51:29 -0300

    Dev4Tech

---
+++ Desktop/Dev4Tech/Dev4Tech/conexao.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Biblioteca de conexão do SQL
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Dev4Tech
+{
+    class conexao
+    {
+        public MySqlConnection conectar;
+        public string servidor, database, usuario, senha;
+
+        public conexao()
+        {
+            inicializar();
+        }
+        public void inicializar()
+        {
+            servidor = "127.0.0.1";
+            /*127.0.0.1 - número do servidor local ou seja o proprio computador, para conexões online necessário colocar o número do servidor na internet.*/
+            database = "crud_csharp";
+            //Database que iremos criar futuramente 
+            usuario = "root";
+            senha = "";
+            /*Usuário e senha são padrões (root e senha branco) para conexões remotas, necessários substituir por usuario e senhas fornecidos */
+            string conexaoString;
+            conexaoString = "SERVER=" + servidor + ";" + "DATABASE= " + database + ";" + "UID= " + usuario + ";" + "PASSWORD= " + senha + ";";
+            conectar = new MySqlConnection(conexaoString);
+            //MySqlConnection - utlizar a string conexaoString para conectar ao banco de dados
+        }
+        public bool abrirConexao()
+        {
+            //try catch é  um tratamento de erros para códigos
+            try
+            {
+                conectar.Open();
+                return true;
+            }
+            catch (MySqlException ex) // Catch - caso o try não execute, o catch entra em ação
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        System.Windows.Forms.MessageBox.Show("Não foi possível conectar.");
+                        break;
+                    case 1045:
+                        System.Windows.Forms.MessageBox.Show("Usuário e senha inválidos");
+                        break;
+                }
+                return false;
+            }
+        }
+        public bool fecharConexao()
+        {
+            try
+            {
+                conectar.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+    }
+}
+)
+
+    }
+}
---
+++ Desktop/Dev4Tech/Dev4Tech/empresa.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Biblioteca de conexão do SQL
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Dev4Tech
+{
+    class empresa : conexao
+    {
+        private string nomeEmpresa, codigoId, CNPJ, setorEmpresarial, logradouro, numResidencia, bairro, rua, complemento;
+
+        public void setNomeEmpresa(string nomeEmpresa)
+        {
+            this.nomeEmpresa = nomeEmpresa;
+        }
+        public void setCodigoId(string codigoId)
+        {
+            this.codigoId = codigoId;
+        }
+        public void setCNPJ(string CNPJ)
+        {
+            this.CNPJ = CNPJ;
+        }
+        public void seSetorEmpresarial(string setorEmpresarial)
+        {
+            this.setorEmpresarial = setorEmpresarial;
+        }
+        public void setLogradouro(string logradouro)
+        {
+            this.logradouro = logradouro;
+        }
+        public void setNumResidencia(string numResidencia)
+        {
+            this.numResidencia = numResidencia;
+        }
+        public void setBairro(string bairro)
+        {
+            this.bairro = bairro;
+        }
+        public void setRua(string rua)
+        {
+            this.rua = rua;
+        }
+        public void setComplemento(string complemento)
+        {
+            this.complemento = complemento;
+        }
+
+        public string getNomeEmpresa()
+        {
+            return this.nomeEmpresa;
+        }
+        public string getCodigoId()
+        {
+            return this.codigoId;
+        }
+        public string getCNPJ()
+        {
+            return this.CNPJ;
+        }
+        public string getSetorEmpresarial()
+        {
+            return this.setorEmpresarial;
+        }
+        public string getLogradouro()
+        {
+            return this.logradouro;
+        }
+        public string getNumResidencia()
+        {
+            return this.numResidencia;
+        }
+        public string getBairro()
+        {
+            return this.bairro;
+        }
+        public string getRua()
+        {
+            return this.rua;
+        }
+        public string getComplemento()
+        {
+            return this.complemento;
+        }
+
+
+        //Método inserir, para mandar os dados no banco de dados
+
+        public void inserir()
+        {
+            string query = "Insert into Cachorro(nome, codigo_id, sexo, idade, pelagem, porte) values ('" + getRaça() + "', '" + getCodigoId() + "', '" + getSexo() + "', '" + getIdade() + "', '" + getPelagem() + "', " + getPorte() + "')";
+
+            if (this.abrirConexao() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, conectar);
+                cmd.ExecuteNonQuery();
+                this.fecharConexao();
+            }
+        }
+
+        //Excluir informações do banco de dados por meio da chave primária
+        public void excluir()
+        {
+            string query = "delete from Cachorro where CodigoId = '" + getCodigoId() + "'";
+            MySqlCommand cmd = new MySqlCommand(query, conectar);
+            cmd.ExecuteNonQuery();
+            this.fecharConexao();
+        }
+
+        //Método cosultar mostra todos os dados existentes na tabela
+        public DataTable Consultar()
+        {
+            this.abrirConexao();
+            string mSQL = "select * from Cachorro";
+            MySqlCommand cmd = new MySqlCommand(mSQL, conectar);
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+
+            this.fecharConexao();
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+    }
+}
+
+    }
+}
