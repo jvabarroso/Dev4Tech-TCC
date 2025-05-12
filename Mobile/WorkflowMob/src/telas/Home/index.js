fe43242549c3171347ba733f43deddd19123504f
GabrielKenzoT <gabrieltakeuchi418@gmail.com>
2025-05-12 02:19:33 -0300

    Design de Home Completo

--- Mobile/WorkflowMob/src/telas/Home/index.js
+++ Mobile/WorkflowMob/src/telas/Home/index.js
@@ -13,43 +13,68 @@ export default function Home({navigation}){
                     style={styles.foto}
                     source={require('../../../assets/img/fotoexemplo.png')} >
                     </Image>
-                    <Text style={styles.nome}>Ramon Trigon</Text>
-                    <Text style={styles.profissao}>Professor</Text>
+                    <View style={styles.verde}></View>
+
+                    <View style={styles.textoperfil}>
+                        <Text style={styles.nome}>Ramon Trigon</Text>
+                        <Text style={styles.profissao}>Professor</Text>
+                    </View>
                 </View>
 
                 <View style={styles.areatitulo}>
-                    <Text style={styles.titulo}>HOME</Text>
+                    <Text style={styles.titulo}>Home</Text>
                     <Text style={styles.subtitulo}>Explore as Ferramentas</Text>
                 </View>
 
                 <View style={styles.areacard}>
                     <Card style={styles.cardtarequi}>
-                        <Card.Cover source={{ uri: 'https://via.placeholder.com/150' }} />
+                        <Card.Cover 
+                            source={require('../../../assets/img/equipes.png')}
+                            style={styles.imagemcard} />
                         <Card.Content
-                            onPress={()=> navigation.navigate('Equipes')} 
+                            onPress={()=> navigation.navigate('Equipes')}
+                            style={styles.cardinferior}
                         >
-                            <Title>Equipes</Title>
-                            <Paragraph>Loremijadsjknadnsakjdnasdnkjldsa</Paragraph>
+                            <Title style={styles.titulocard}>Equipes</Title>
+                            <Paragraph style={styles.paragraph}>The point of using Lorem Ipsum is that....</Paragraph>
+                            <View style={styles.linhainfer}>
+                                <Text style={styles.data}>16/07/20</Text>
+                                <Text style={styles.Entre}>Entre aqui</Text>
+                            </View>
                         </Card.Content>
                     </Card>
 
                     <Card style={styles.cardtarequi}>
-                        <Card.Cover source={{ uri: 'https://via.placeholder.com/150' }} />
+                        <Card.Cover 
+                            source={require('../../../assets/img/tarefas.png')} 
+                            style={styles.imagemcard}/>
                         <Card.Content
-                            onPress={()=> navigation.navigate('TarefasPedentes')} 
+                            onPress={()=> navigation.navigate('Tarefas')} 
+                            style={styles.cardinferior}
                         >
-                            <Title>Tarefas</Title>
-                            <Paragraph>daugduiahdiuahsudh</Paragraph>
+                            <Title style={styles.titulocard}>Tarefas</Title>
+                            <Paragraph style={styles.paragraph}>The point of using Lorem Ipsum is that....</Paragraph>
+                            <View style={styles.linhainfer}>
+                                <Text style={styles.data}>16/07/20</Text>
+                                <Text style={styles.Entre}>Entre aqui</Text>
+                            </View>
                         </Card.Content>
                     </Card>
 
-                    <Card style={styles.cardraking}>
-                        <Card.Cover source={{ uri: 'https://via.placeholder.com/150' }} />
+                    <Card style={styles.cardtarequi}>
+                        <Card.Cover 
+                            source={require('../../../assets/img/raking.png')} 
+                            style={styles.imagemcard}/>
                         <Card.Content
                             onPress={()=> navigation.navigate('Raking')} 
+                            style={styles.cardinferior}
                         >
-                            <Title>Tela de Raking</Title>
-                            <Paragraph>sdsdfsfsdffsafds.</Paragraph>
+                            <Title style={styles.titulocard}>Raking</Title>
+                            <Paragraph style={styles.paragraph}>The point of using Lorem Ipsum is that....</Paragraph>
+                            <View style={styles.linhainfer}>
+                                <Text style={styles.data}>16/07/20</Text>
+                                <Text style={styles.Entre}>Entre aqui</Text>
+                            </View>
                         </Card.Content>
                     </Card>
                 </View>
@@ -73,7 +98,6 @@ const styles = StyleSheet.create({
   areaperfil:{
     flexDirection: 'row',
     marginTop: 15,
-    justifyContent: 'space-between',
     width: '100%',
     paddingHorizontal: 20,
   },
@@ -83,16 +107,82 @@ const styles = StyleSheet.create({
     marginLeft: 18,
     borderRadius: "100%"
   },
-  nome:{
-    color:'#000',
+  verde:{
+    width:20,
+    height:20,
+    borderRadius:"100%",
+    borderWidth: 3.5,
+    borderColor: '#F5F9F9',
+    backgroundColor:"#2EBA4E",
+    right:28,
+    top:95,
+  },
+  textoperfil: {
+    justifyContent: 'center',
+    marginLeft: 20, 
+  },
+  nome: {
+    color: '#000',
+    fontSize: 16,
+    marginBottom: 2,
+  },
+  profissao: {
+    color: '#aaaaaa',
+    fontSize: 16,
+  },
+  areatitulo:{
+    justifyContent: 'center',
+    marginRight: 50,
+    marginTop: 20,
+    marginBottom: 30,
+  },
+  titulo:{
+    fontSize: 24,
+    marginBottom: 4,
+  },
+  subtitulo:{
     fontSize: 20,
+  },
+  areacard:{
+    marginBottom: 25,
+  },
+  cardtarequi:{
+    marginBottom: 25,
+    width:275,
+  },
+  imagemcard:{
+    height:110,
+  },
+  cardinferior:{
+    backgroundColor: "#ffffffff",
+    borderRadius:8,
+    borderBottomWidth: 0.1,
+    borderBottomColor: '#000',
+  },
+  titulocard:{
+    color:"#000",
     fontWeight: 'bold',
-    right:30,
+    marginBottom: -5,
   },
-  profissao:{
-    color:'#aaaaaa',
-    fontSize: 18,
-    left:5
+  paragraph:{
+    color:"#000",
+    fontSize: 11,
   },
-
+  linhainfer:{
+    flexDirection: 'row',
+    marginTop: 11,
+    justifyContent: 'space-between',
+    width: '100%',
+    paddingHorizontal: -10,
+    marginBottom: -10,
+  },
+  data:{
+    fontSize: 11,
+    color: '#aaaaaa',
+  },
+  Entre:{
+    color:"#000",
+    fontSize: 11,
+    fontWeight: 'bold',
+  },
 })
