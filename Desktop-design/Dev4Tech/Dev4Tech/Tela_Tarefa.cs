99d8062bab702121c5e5ed06b0869e8e834054ef
João Vitor Alves Barroso <jvabarroso@gmail.com>
2025-11-22 22:48:42 -0300

    correcao-caminho-arquivos

--- Desktop-design/Dev4Tech/Dev4Tech/Tela_Tarefa.cs
+++ Desktop-design/Dev4Tech/Dev4Tech/Tela_Tarefa.cs
@@ -237,7 +237,7 @@ namespace Dev4Tech
                 return;
             }
 
-            string pastaArquivos = @"C:\Dev4Tech\ArquivosEntregas";
+            string pastaArquivos = @"C:\xampp\htdocs\dev4tech\arquivos";
             if (!Directory.Exists(pastaArquivos))
                 Directory.CreateDirectory(pastaArquivos);
 
